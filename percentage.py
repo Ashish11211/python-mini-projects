21bed3957efda6fe532a27d055e3d41a64efc08e
Ashish ranjan <128678230+Ashish11211@users.noreply.github.com>
2026-05-15 11:34:30 +0530

    Add percentage calculation program

---
+++ percentage.py
@@ -0,0 +1,10 @@
+---Percentage Program------
+math = int(input("Math marks: "))
+science = int(input("Science marks: "))
+english = int(input("English marks: "))
+
+total = math + science + english
+percentage = total / 3
+
+print("Total =", total)
+print("Percentage =", percentage)
