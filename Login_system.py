063c0ce8f2091c7d1feb613c5da5a4fb0009c467
Ashish ranjan <128678230+Ashish11211@users.noreply.github.com>
2026-05-15 11:47:52 +0530

    Create Login_system.py

---
+++ Login_system.py
@@ -0,0 +1,11 @@
+username = "admin"
+password = "1234"
+
+user = input("Enter username: ")
+passw = input("Enter password: ")
+
+if user == username and passw == password:
+    print("Login Successful")
+
+else:
+    print("Invalid Username or Password")
