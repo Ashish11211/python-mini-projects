2f138f163d05d7b7a58af8d53d1b036b68ed451d
Ashish ranjan <128678230+Ashish11211@users.noreply.github.com>
2026-05-15 11:35:33 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter your number1: "))
+num2 = int(input("enter your number2: "))
+
+print("ad=", num1 + num2)
+print("sub=", num1 - num2)
+print("multi=", num1 * num2)
+print("division=", num1 / num2)
